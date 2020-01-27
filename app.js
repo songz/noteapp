cd55d98c85762ec09a4a0c853b798e2c7befa4c3
Song Zheng <song@zheng.club>
2020-01-26 19:57:01 -0800

    add views for server side rendering

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const path = require('path')
 const md = require('markdown-it')()
 const simpleGit = require('simple-git/promise')(path.resolve(`${__dirname}/data`))
 const app = express()
-app.use(express.static('public'))
+app.set('views', path.resolve(`${__dirname}/views`))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded())
 app.use(express.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('hello')
+})
+
+app.get('/show/:commit/:name', async (req, res) => {
+  await simpleGit.checkout(req.params.commit)
+  const name = req.params.name
+  fs.readFile(`./data/${name}`, (err, data) => {
+    simpleGit.checkout('master')
+    if (err) {
+      console.log('error', err)
+      return res.sendFile(path.resolve(`${__dirname}/public/error.html`))
+    }
+    const content = md.render(data.toString())
+    res.render('note', { name, content })
+  })
 })
 
 app.post('/api/notes', async (req, res) => {
@@ -31,17 +48,6 @@ app.post('/api/notes', async (req, res) => {
 app.get('/api/logs/:name', async (req, res) => {
   const logs = await simpleGit.log({ file: req.params.name })
   res.json(logs)
-})
-
-app.get('/show/:commit/:name', async (req, res) => {
-  await simpleGit.checkout(req.params.commit)
-  fs.readFile(`./data/${req.params.name}`, (err, data) => {
-    simpleGit.checkout('master')
-    if (err) {
-      return res.sendFile('./public/error.html')
-    }
-    res.send(md.render(data.toString()))
-  })
 })
 
 app.listen(process.env.PORT || 3030) // notes.slacker.club
