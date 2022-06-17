a99e26dce40821ed27654eebbcceb69d3fa381e9
Chi Kim <chi@kim.club>
2022-06-17 05:06:14 -0700

    lint on app.js

--- app.js
+++ app.js
@@ -1,105 +1,111 @@
-const express = require('express')
-const fs = require('fs')
-const util = require('util')
-const path = require('path')
-const multer = require('multer')
-const {setupGit} = require('./lib/git')
-const { spawn } = require('child_process')
-const {renderErrorPage} = require('./lib/render')
-const noteApiRouter = require('./routes/api/notes')
-const noteViewRouter = require('./routes/views/notes')
+const express = require("express");
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const multer = require("multer");
+const { setupGit } = require("./lib/git");
+const { spawn } = require("child_process");
+const { renderErrorPage } = require("./lib/render");
+const noteApiRouter = require("./routes/api/notes");
+const noteViewRouter = require("./routes/views/notes");
 
-const simpleGit = setupGit(path.resolve(`${__dirname}/data`))
+const simpleGit = setupGit(path.resolve(`${__dirname}/data`));
 
-const getFileStat = util.promisify(fs.stat)
+const getFileStat = util.promisify(fs.stat);
 
-const app = express()
+const app = express();
 
-const assetPath = '.noteapp-assets'
-app.set('views', path.resolve(`${__dirname}/views`))
-app.set('view engine', 'ejs')
-app.use(express.urlencoded())
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/assets', express.static(`data/${assetPath}`))
+const assetPath = ".noteapp-assets";
+app.set("views", path.resolve(`${__dirname}/views`));
+app.set("view engine", "ejs");
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/assets", express.static(`data/${assetPath}`));
 
 const storage = multer.diskStorage({
   destination: `data/${assetPath}`,
   filename: function (_, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname.replace(/ /g, '-')}`)
-  }
-})
-const upload = multer({storage})
+    cb(null, `${Date.now()}-${file.originalname.replace(/ /g, "-")}`);
+  },
+});
+const upload = multer({ storage });
 
 const excludedNames = {
-  '.git': true,
-  '.noteapp-assets': true,
-}
+  ".git": true,
+  ".noteapp-assets": true,
+};
 
-app.get('/search/:query', (req, res) => {
-  const child = spawn('rg', [req.params.query, './data', '-im', '1']);
+app.get("/search/:query", (req, res) => {
+  const child = spawn("rg", [req.params.query, "./data", "-im", "1"]);
   child.stdout.pipe(res);
-})
+});
 
-app.post('/api/files', upload.any('assets'), (req, res) => {
-  let allNames = ''
-  const filePaths = req.files.map(f => {
-    allNames += `, ${f.filename}`
+app.post("/api/files", upload.any("assets"), (req, res) => {
+  let allNames = "";
+  const filePaths = req.files.map((f) => {
+    allNames += `, ${f.filename}`;
     return {
       name: f.filename,
       path: `/assets/${f.filename}`,
-      isImage: f.mimetype.includes('image')
-    }
-  })
+      isImage: f.mimetype.includes("image"),
+    };
+  });
   Promise.all(
-    filePaths.map(f => simpleGit.add(`${assetPath}/${f.name}`))
+    filePaths.map((f) => simpleGit.add(`${assetPath}/${f.name}`))
   ).then(() => {
-    simpleGit.commit(`add files ${allNames}`)
-  })
-  res.json(filePaths)
-})
+    simpleGit.commit(`add files ${allNames}`);
+  });
+  res.json(filePaths);
+});
 
-app.get(['/', '/notes'], (req, res) => {
+app.get(["/", "/notes"], (req, res) => {
   fs.readdir(`./data`, async (err, files) => {
     if (renderErrorPage(err, res)) {
-      return
+      return;
     }
-    const notes = files.filter(note => !excludedNames[note])
+    const notes = files.filter((note) => !excludedNames[note]);
 
-    const noteResults = await Promise.all(notes.map(fname => {
-      return getFileStat(`./data/${fname}`)
-    }))
-    const notesInfo = noteResults.map((info, idx) => {
-      return {
-        ...info,
-        name: notes[idx]
-      }
-    }).sort((a, b) => b.mtimeMs - a.mtimeMs)
+    const noteResults = await Promise.all(
+      notes.map((fname) => {
+        return getFileStat(`./data/${fname}`);
+      })
+    );
+    const notesInfo = noteResults
+      .map((info, idx) => {
+        return {
+          ...info,
+          name: notes[idx],
+        };
+      })
+      .sort((a, b) => b.mtimeMs - a.mtimeMs);
 
-    const content = JSON.stringify(notesInfo)
-    res.render('notes', { data: {
-      content,
-      path: 'home'
-    } })
-  })
-})
+    const content = JSON.stringify(notesInfo);
+    res.render("notes", {
+      data: {
+        content,
+        path: "home",
+      },
+    });
+  });
+});
 
-app.get('/raw/:name', async (req, res) => {
-  const notePath = `./data/${req.params.name}`
+app.get("/raw/:name", async (req, res) => {
+  const notePath = `./data/${req.params.name}`;
   fs.readFile(notePath, (err, data) => {
     if (renderErrorPage(err, res)) {
-      return
+      return;
     }
-    res.send( data.toString() )
-  })
-})
+    res.send(data.toString());
+  });
+});
 
-app.use('/notes', noteViewRouter)
-app.use('/api/notes', noteApiRouter)
+app.use("/notes", noteViewRouter);
+app.use("/api/notes", noteApiRouter);
 
-app.get('/api/logs/:name', async (req, res) => {
-  const logs = await simpleGit.log({ file: req.params.name })
-  res.json(logs)
-})
+app.get("/api/logs/:name", async (req, res) => {
+  const logs = await simpleGit.log({ file: req.params.name });
+  res.json(logs);
+});
 
-app.listen(process.env.PORT || 3030) // notes.slacker.club
+app.listen(3030); // notes.slacker.club
